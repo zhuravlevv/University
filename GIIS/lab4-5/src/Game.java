eb98066c29c93fec5585925a3a1763efb160287a
Vlad Zhuravlev <vlad12.09.01.1@mail.ru>
2021-11-07 20:34:32 +0300

    add GIIS lab-45

---
+++ GIIS/lab4-5/src/Game.java
@@ -0,0 +1,249 @@
+import javax.imageio.ImageIO;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.util.Random;
+
+public class Game extends JPanel {
+    private JLabel[][]  squares;
+    private JLabel[][]  squaresEtalon;
+    private int row;
+    private int col;
+    public int sizeGame = 4 ;
+    public int sizeBlock = 150;
+    MouseHandler handler = new MouseHandler();
+    public boolean win = false;
+    public BufferedImage img;
+    public BufferedImage[][] images;
+    private void initialize(){
+        setSize(sizeBlock*sizeBlock,sizeBlock*sizeBlock);
+        row = sizeGame -1;
+        col = sizeGame -1;
+        squaresEtalon = new JLabel[sizeGame][sizeGame];
+        squares = new JLabel[sizeGame][sizeGame];
+        setLayout(new GridLayout(sizeGame,sizeGame,1,1));
+    }
+    private void createPuzzle(){
+        for (int i = 0; i < sizeGame; i++) {
+            for (int j = 0; j < sizeGame; j++) {
+                add(squares[i][j]);
+            }
+        }
+    }
+    public Game() {
+        initialize();
+        createNumberGame();
+        shuffle();
+        createPuzzle();
+    }
+    public Game(String src){
+        initialize();
+        createImageGame(src);
+        shuffle();
+        createPuzzle();
+    }
+    private void createNumberGame(){
+        JLabel sq;
+        for (int i = 0; i < sizeGame; i++) {
+            for (int j = 0; j < sizeGame; j++) {
+                if(i != sizeGame-1 || j != sizeGame-1) {
+                    sq = new JLabel("<html>" +
+                            "<font size='18' color='black'><strong>" + (i * sizeGame + j + 1) + "</strong></font>" +
+                            "</html>");
+                    sq.setVerticalAlignment(JLabel.CENTER);
+                    sq.setHorizontalAlignment(JLabel.CENTER);
+                    sq.setBackground(Color.gray);
+                }
+                else{
+                    sq = new JLabel();
+                    sq.setBackground(Color.black);
+                }
+                sq.setPreferredSize(new Dimension(sizeBlock,sizeBlock));
+                sq.setOpaque(true);
+                squaresEtalon[i][j] = sq;
+                sq.addMouseListener(handler);
+                squares[i][j] = sq;
+            }
+        }
+    }
+    private void createImageGame(String src){
+        try {
+            img = ImageIO.read(new File(src));
+            Integer width = img.getWidth()/sizeGame;
+            Integer height = img.getHeight()/sizeGame;
+            images = new BufferedImage[sizeGame][sizeGame];
+            int black = Color.black.getRGB();
+            JLabel sq;
+            for (int i = 0; i < sizeGame; i++) {
+                for (int j = 0; j < sizeGame; j++) {
+                    sq = new JLabel();
+                    images[i][j] = new BufferedImage(width,height, BufferedImage.TYPE_INT_ARGB);
+                    for (int k = 0 + i*height,q = 0; k < height + i*height; k++,q++) {
+                        for (int l = 0 + j*width,w = 0; l < width + j*width; l++,w++) {
+                            if(i!=sizeGame-1||j!=sizeGame-1)
+                                images[i][j].setRGB(w,q,img.getRGB(l,k));
+                            else
+                                images[i][j].setRGB(w,q,black);
+                        }
+                    }
+                    sq.setSize(new Dimension(sizeBlock,sizeBlock));
+                    sq.setIcon(new ImageIcon(resize(images[i][j],sq)));
+
+                    squaresEtalon[i][j] = sq;
+                    sq.addMouseListener(handler);
+                    squares[i][j] = sq;
+                }
+            }
+        }catch (IOException e){
+            e.printStackTrace();
+        }
+    }
+
+    private void shuffle(){
+        Random rnd = new Random();
+        Integer s = sizeGame - 1;
+        String[] moves = {"up","down","left","right"};
+        for (int i = 0; i < 100; i++) {
+            move(moves[rnd.nextInt(2)]);
+            move(moves[rnd.nextInt(2) + 2]);
+        }
+    }
+    public void checkWin() {
+        for (int i = 0; i < sizeGame; i++) {
+            for (int j = 0; j < sizeGame; j++) {
+                if (!squaresEtalon[i][j].equals(squares[i][j]))
+                    return;
+            }
+        }
+        win = true;
+        for (int i = 0; i < sizeGame; i++)
+            for (int j = 0; j < sizeGame; j++) {
+                squares[i][j].removeMouseListener(handler);
+            }
+        JOptionPane.showMessageDialog(Game.this,"You Win!!!");
+
+    }
+    public BufferedImage resize(BufferedImage img, JLabel panel){
+        Image tmp = img.getScaledInstance(panel.getWidth(), panel.getHeight(), Image.SCALE_SMOOTH);
+        img = new BufferedImage(panel.getWidth(), panel.getHeight(), BufferedImage.TYPE_INT_ARGB);
+
+        Graphics2D g2d = img.createGraphics();
+        g2d.drawImage(tmp, 0, 0, null);
+        g2d.dispose();
+        return img;
+    }
+    public void move(String key){
+        switch (key){
+            case "up":{
+                if(row == sizeGame-1)
+                    return;
+                else{
+                    row++;
+                    swap(row,col,row-1,col);
+                    break;
+                }
+            }
+            case "down":{
+                if(row == 0)
+                    return;
+                else{
+                    row--;
+                    swap(row,col,row+1,col);
+                    break;
+                }
+            }
+            case "left":{
+                if(col == sizeGame-1)
+                    return;
+                else{
+                    col++;
+                    swap(row,col,row,col-1);
+                    break;
+                }
+            }
+            case "right":{
+                if(col == 0)
+                    return;
+                else{
+                    col--;
+                    swap(row,col,row,col+1);
+                    break;
+                }
+            }
+            default:
+                return;
+        }
+
+        paint();
+    }
+
+    private void swap(Integer rowNew, Integer colNew, Integer rowOld, Integer colOld){
+        JLabel t = squares[rowNew][colNew];
+        squares[rowNew][colNew] = squares[rowOld][colOld];
+        squares[rowOld][colOld] = t;
+    }
+    private void paint(){
+        removeAll();
+        for (int i = 0; i < sizeGame; i++)
+            for (int j = 0; j < sizeGame; j++)
+                add(squares[i][j]);
+        revalidate();
+        repaint();
+    }
+
+    private class MouseHandler implements MouseListener{
+
+        private void handleClick(Integer i, Integer j){
+            if(row == i && col == j+1) {
+                col--;
+                swap(row, col+1, i, j);
+            }
+            if(row == i && col == j-1){
+                col++;
+                swap(row, col-1, i, j);
+            }
+            else if(row == i+1 && col == j){
+                row--;
+                swap(row+1, col, i, j);
+            }
+            else if(row == i-1 && col == j){
+                row++;
+                swap(row-1, col, i, j);
+            }
+            else return;
+            checkWin();
+        }
+        @Override
+        public void mouseClicked(MouseEvent e) {
+            Object source = e.getSource();
+            for (int i = 0; i < sizeGame; i++)
+                for (int j = 0; j < sizeGame; j++)
+                    if(source.equals(squares[i][j])) {
+                        handleClick(i, j);
+                        paint();
+                        return;
+                    }
+        }
+
+        @Override
+        public void mousePressed(MouseEvent e) {
+        }
+
+        @Override
+        public void mouseReleased(MouseEvent e) {
+        }
+
+        @Override
+        public void mouseEntered(MouseEvent e) {
+        }
+
+        @Override
+        public void mouseExited(MouseEvent e) {
+        }
+    }
+
+}
---
+++ GIIS/lab4-5/src/MainFrame.java
@@ -0,0 +1,291 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class MainFrame extends JFrame{
+    Integer scoreGame = 0;
+    Integer scoreGameNumber= 0;
+    Integer scoreGameImage= 0;
+    Integer scoreGameNumberWin= 0;
+    Integer scoreGameImageWin= 0;
+    String imageSrc;
+    String statisticFile = "/home/vladislav/IdeaProjects/University/GIIS/lab4-5/src/resources/statistics.txt";
+    Game mainPanel;
+    String mode;
+    boolean win = false;
+    boolean win2 = true;
+    public MainFrame(){
+        setJMenuBar(createMenu());
+        loadStatistics();
+        imageSrc = "/home/vladislav/IdeaProjects/University/GIIS/lab4-5/src/resources/";
+        mode = "numbers";
+        win = false;
+        win2 = true;
+        mainPanel = new Game();
+        scoreGame++;
+        scoreGameNumber++;
+        add(mainPanel);
+        addWindowListener(new WindowAdapter(){
+            @Override
+            public void windowClosing(WindowEvent ev){
+                closeWindow();
+            }
+        });
+        addKeyListener(new KeyAdapter() {
+            @Override
+            public void keyPressed(KeyEvent evt) {
+                if(win2) {
+                    checkWin();
+                }
+                if(win2) {
+                    switch (evt.getKeyCode()) {
+                        case KeyEvent.VK_UP:
+                            mainPanel.move("up");
+                            break;
+                        case KeyEvent.VK_DOWN:
+                            mainPanel.move("down");
+                            break;
+                        case KeyEvent.VK_LEFT:
+                            mainPanel.move("left");
+                            break;
+                        case KeyEvent.VK_RIGHT:
+                            mainPanel.move("right");
+                            break;
+                    }
+                    mainPanel.checkWin();
+                    checkWin();
+                }
+            }
+        });
+    }
+    private void checkWin() {
+        if (mainPanel.win) {
+            win = true;
+            mainPanel.win = false;
+        }
+
+        if (win == true) {
+            if (mode.equals("numbers"))
+                scoreGameNumberWin++;
+            else
+                scoreGameImageWin++;
+            win = false;
+            win2 = false;
+        }
+    }
+    private boolean newGame(String str){
+        int res = JOptionPane.showConfirmDialog(MainFrame.this,"Start new game" + str + "?",
+                "New game", JOptionPane.YES_NO_OPTION);
+        if(res == JOptionPane.YES_OPTION){
+            remove(mainPanel);
+            if(mode.equals("numbers")) {
+                mainPanel = new Game();
+                scoreGameNumber ++;
+            }
+            if(mode.equals("image")) {
+                mainPanel = new Game(imageSrc);
+                scoreGameImage ++;
+            }
+            scoreGame ++;
+            win = false;
+            win2 = true;
+            add(mainPanel);
+            checkWin();
+            pack();
+        }
+        else
+            return false;
+        return true;
+    }
+    private JMenuBar createMenu(){
+        JMenuBar menu = new JMenuBar();
+        JMenu game = new JMenu("Game");
+        JMenuItem newGame = new JMenuItem("New Game");
+        newGame.addActionListener(e -> newGame(""));
+        JMenuItem statistics = new JMenuItem("Statistics");
+        statistics.addActionListener(e -> {
+            checkWin();
+            JOptionPane.showMessageDialog(MainFrame.this,
+                    new String[]{"Games played: " + scoreGame + " -> Win: " + (scoreGameNumberWin + scoreGameImageWin),
+                            "Games played with numbers: " + scoreGameNumber + " -> Win: " + scoreGameNumberWin,
+                            "Games played with image: " + scoreGameImage + " -> Win: " + scoreGameImageWin
+                    }, "Statistics", JOptionPane.INFORMATION_MESSAGE);
+        });
+        JMenuItem exit = new JMenuItem("Exit");
+        exit.addActionListener(e -> {
+            closeWindow();
+        });
+        game.add(newGame);
+        game.add(statistics);
+        game.addSeparator();
+        game.add(exit);
+
+        JMenu settings = new JMenu("Settings");
+        JRadioButtonMenuItem numbers = new JRadioButtonMenuItem("Numbers");
+        JRadioButtonMenuItem image = new JRadioButtonMenuItem("Image");
+        numbers.setEnabled(false);
+        numbers.setSelected(true);
+        numbers.addActionListener(e -> {
+            numbers.setEnabled(false);
+            image.setSelected(false);
+            mode = "numbers";
+            if(newGame(" with numbers") == false){
+                mode = "image";
+                image.setSelected(true);
+                numbers.setSelected(false);
+                numbers.setEnabled(true);
+            }
+            else
+                image.setEnabled(true);
+        });
+        image.addActionListener(e -> {
+            image.setEnabled(false);
+            numbers.setSelected(false);
+            mode = "image";
+            if(newGame(" with image") == false){
+                mode = "numbers";
+                numbers.setSelected(true);
+                image.setSelected(false);
+                image.setEnabled(true);
+            }
+            else
+                numbers.setEnabled(true);
+        });
+
+        JMenuItem loadImage = new JMenuItem("Load Image");
+        loadImage.addActionListener(e -> {
+            checkWin();
+            JFileChooser c = new JFileChooser();
+            c.setCurrentDirectory(new File("src//lab1"));
+            int rVal = c.showOpenDialog(MainFrame.this);
+            if (rVal == JFileChooser.APPROVE_OPTION) {
+                if(c.getSelectedFile().toString().contains(".jpg") || c.getSelectedFile().toString().contains(".png")){
+                    JOptionPane.showMessageDialog(MainFrame.this,"Wait");
+                    remove(mainPanel);
+                    mainPanel = new Game(c.getSelectedFile().toString());
+                    imageSrc = c.getSelectedFile().toString();
+                    image.setEnabled(false);
+                    numbers.setSelected(false);
+                    image.setSelected(true);
+                    mode = "image";
+                    numbers.setEnabled(true);
+                    scoreGameImage++;
+                    scoreGame++;
+                    add(mainPanel);
+                    pack();
+                    JOptionPane.showMessageDialog(MainFrame.this,"Load success");
+                }
+            }
+            else if (rVal == JFileChooser.CANCEL_OPTION)
+                JOptionPane.showMessageDialog(MainFrame.this,"Load cancel",
+                        "", JOptionPane.OK_OPTION);
+            else
+                JOptionPane.showMessageDialog(MainFrame.this,"Pick image in jpg or png format",
+                        "Error", JOptionPane.OK_OPTION);
+
+        });
+
+        settings.add(numbers);
+        settings.add(image);
+        settings.add(loadImage);
+        JMenu help = new JMenu("Help");
+        JMenuItem rules = new JMenuItem("Rules");
+        rules.addActionListener(e -> {
+            checkWin();
+            JOptionPane.showMessageDialog(MainFrame.this,
+                    new String[] {"Цель игры: перемещая костяшки по коробке,",
+                            "добиться упорядочивания их по номерам от 1 до 15, ",
+                            "желательно, сделав как можно меньше перемещений."
+                    },"Rules",JOptionPane.INFORMATION_MESSAGE);
+        });
+        JMenuItem about = new JMenuItem("About");
+        about.addActionListener(e -> {
+            checkWin();
+            JOptionPane.showMessageDialog(MainFrame.this,
+                    new String[] {"GIIS 4-5",
+                            "Пятнашки"
+                    },"About",JOptionPane.INFORMATION_MESSAGE);
+        });
+        help.add(rules);
+        help.add(about);
+
+        menu.add(game);
+        menu.add(settings);
+        menu.add(help);
+        return menu;
+    }
+    private void closeWindow(){
+        checkWin();
+        saveStatistics();
+        setVisible(false);
+        dispose();
+    }
+    private void loadStatistics() {
+        try {
+            File file = new File(statisticFile);
+            if (file.exists()) {
+                ArrayList<String> list = new ArrayList<>();
+                Pattern p = Pattern.compile("(\\d+).+(\\d+)");
+                Files.lines(Paths.get(statisticFile), StandardCharsets.UTF_8)
+                        .forEach(e -> list.add(e));
+                Matcher m = p.matcher(list.get(1));
+                if(m.find())
+                    scoreGame = Integer.parseInt(m.group(1));
+                m = p.matcher(list.get(2));
+                if(m.find()){
+                    scoreGameNumber = Integer.parseInt(m.group(1));
+                    scoreGameNumberWin = Integer.parseInt(m.group(2));
+                }
+                m = p.matcher(list.get(3));
+                if(m.find()){
+                    scoreGameImage = Integer.parseInt(m.group(1));
+                    scoreGameImageWin = Integer.parseInt(m.group(2));
+                }
+
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+    private void saveStatistics(){
+        try {
+            File file = new File(statisticFile);
+            file.createNewFile();
+            try (FileWriter writer = new FileWriter(file)) {
+                writer.write("Statistics:\n");
+                writer.write("Games played: "+ scoreGame + " -> Win: "+ (scoreGameNumberWin + scoreGameImageWin) + "\n");
+                writer.write("Games played with numbers: "+ scoreGameNumber +" -> Win: "+ scoreGameNumberWin + "\n");
+                writer.write("Games played with image: " + scoreGameImage + " -> Win: "+ scoreGameImageWin + "\n");
+                writer.flush();
+            }
+        }
+        catch (IOException e){
+            e.printStackTrace();
+        }
+    }
+    public static void main(String[] args) {
+        JFrame frame = new MainFrame();
+        frame.setSize(500,500);
+        frame.setLocationRelativeTo(null);
+        frame.setTitle("Magic 15");
+        frame.setVisible(true);
+        frame.setResizable(false);
+        frame.setLayout(new GridBagLayout());
+        frame.pack();
+
+
+
+    }
+}
